cd71876317fc63f15ae589d6b3b02553b1de49fb
Ahmad Mazloum <ahmadmazloum98@gmail.com>
2018-05-11 17:35:28 +0200

    sprites <3

--- src/model/Tilee.java
+++ src/model/Tilee.java
@@ -18,17 +18,37 @@ public class Tilee  extends Rectangle{
         setHeight(31);
 
         relocate(x *20, y * 20);
-        
+    	InputStream is;
         if(c.getType()=="Stone")
             setFill(Color.BLACK);
-        else if(c.getType()=="EmptyTile")
-        	  setFill( Color.MEDIUMAQUAMARINE);
+        else if(c.getType()=="EmptyTile") {
+        	if(x==30&&y==28) {
+        		
+        	 	
+            	try {
+    				is = Files.newInputStream(Paths.get("sprites/gobletoffire.jpg"));
+    				Image img = new Image(is);
+    		        setFill(new ImagePattern(img));
+    	        	
+    			} catch (IOException e) {
+    System.out.println("eroor in image");
+    				e.printStackTrace();
+        		
+        		
+        }
+        		
+        	}
+        	
+        else {	setFill( Color.MEDIUMAQUAMARINE);}
+        	
+        }
+        	  
         else if(c.getType()=="Trees")
              setFill(Color.DARKGREEN);
-        else if(c.getType()=="Player")  /*{
-       	InputStream is;
+        else if(c.getType()=="Player")  {
+       	//InputStream is;
 			try {
-				is = Files.newInputStream(Paths.get("Screen Shot 2018-05-09 at 4.42.52 PM.jpg"));
+				is = Files.newInputStream(Paths.get("sprites/Screen Shot 2018-05-09 at 4.42.52 PM.jpg"));
 				Image img = new Image(is);
 		        setFill(new ImagePattern(img));
 	        	
@@ -43,18 +63,78 @@ System.out.println("eroor in image");
         	
         	
         	
-        }*/
-             setFill( Color.BLUE);
-        else if(c.getType()=="HealthGift")
-             setFill( Color.ANTIQUEWHITE);
-        else if(c.getType()=="BulletsGift")
-             setFill( Color.SEASHELL);
+        }
+             //setFill( Color.BLUE);
+        else if(c.getType()=="HealthGift") {
+        	
+         	try {
+				is = Files.newInputStream(Paths.get("sprites/Screen Shot 2018-05-11 at 5.30.59 PM.png"));
+				Image img = new Image(is);
+		        setFill(new ImagePattern(img));
+	        	
+			} catch (IOException e) {
+System.out.println("eroor in image");
+				e.printStackTrace();
+    		
+    		
+    }
+        	
+        	
+        	
+        }
+            // setFill( Color.ANTIQUEWHITE);
+        else if(c.getType()=="BulletsGift"){
+        		try {
+    				is = Files.newInputStream(Paths.get("sprites/bullets.jpg"));
+    				Image img = new Image(is);
+    		        setFill(new ImagePattern(img));
+    	        	
+    			} catch (IOException e) {
+    System.out.println("eroor in image");
+    				e.printStackTrace();
+        		
+        		
+        }}
+            // setFill( Color.SEASHELL);
         else  if(c.getType()=="ArmorGift")
              setFill( Color.RED);
-        else if(c.getType()=="SmallBomb")
-             setFill( Color.ROSYBROWN);
-        else if(c.getType()=="BigBomb")
-             setFill( Color.BLUEVIOLET);
+        else if(c.getType()=="SmallBomb") {
+        	
+        	
+        	try {
+				is = Files.newInputStream(Paths.get("sprites/smallbomb.jpg"));
+				Image img = new Image(is);
+		        setFill(new ImagePattern(img));
+	        	
+			} catch (IOException e) {
+System.out.println("eroor in image");
+				e.printStackTrace();
+    		
+    		
+    }
+        	
+        	
+        	
+        }
+            // setFill( Color.ROSYBROWN);
+        else if(c.getType()=="BigBomb") {
+        	
+        	
+        	
+        	
+        	try {
+				is = Files.newInputStream(Paths.get("sprites/bigbomb.jpg"));
+				Image img = new Image(is);
+		        setFill(new ImagePattern(img));
+	        	
+			} catch (IOException e) {
+System.out.println("eroor in image");
+				e.printStackTrace();
+    		
+    		
+    }
+        }
+            // setFill( Color.BLUEVIOLET);
     }
 }
 
